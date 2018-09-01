da838d0d52157f3e1e75ba44940ce88393365eae
Duck <duckalini@gmail.com>
2018-09-01 12:26:58 +1200

    Add developer IAM set up

--- modules/iam/developer_group.tf
+++ modules/iam/developer_group.tf
@@ -1,72 +1,85 @@
 // Developer group set up
 
 resource "aws_iam_group" "developers" {
-  name = "Developers"
+  name = "Developer"
 }
 
-// Allow users in the developer group to assume the developer role
-resource "aws_iam_group_policy" "allow_developer_group_to_assume_role" {
-  group = "${aws_iam_group.developers}"
-  policy = "${aws_iam_policy.allow_developer_group_to_assume_role.arn}"
+resource "aws_iam_policy" "developer_access" {
+  name = "DeveloperAccess"
+  description = "Developer AWS access rights"
+  policy = "${data.aws_iam_policy_document.developer_access.json}"
 }
 
-resource "aws_iam_policy" "allow_developer_group_to_assume_role" {
-  policy = "${data.aws_iam_policy_document.allow_groups_to_assume_developer_role.json}"
-}
-
-data "aws_iam_policy_document" "allow_groups_to_assume_developer_role" {
-    statement {
-    actions = [
-      "sts:AssumeRole",
-    ]
-
-    resources = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/developer_role",
-    ]
-  }
-}
-
-// Role set up
-
-resource "aws_iam_role" "developer_role" {
-  assume_role_policy = "${aws_iam_policy.developer_assume_role.arn}"
-}
-
-resource "aws_iam_role_policy" "developer_role_trust_policy" {
-  policy = "${aws_iam_policy.developer_role_trust_policy.arn}"
-  role = "${aws_iam_role.developer_role.id}"
-}
-
-// Assume role set up
-data "aws_iam_policy_document" "developer_role_trust_policy" {
+// Allow developer group to assume the developer role
+data "aws_iam_policy_document" "developer_users_can_assume_roles" {
   statement {
+    effect  = "Allow"
     actions = ["sts:AssumeRole"]
 
-    principals {
-      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
-      type = "AWS"
+    resources = [
+      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/dev",
+    ]
+
+    condition = {
+      test     = "Bool"
+      variable = "aws:MultiFactorAuthPresent"
+      values   = ["true"]
     }
   }
 }
 
+// Create developer role with developer permissions
+resource "aws_iam_role" "dev" {
+  name = "dev"
+  path = "/"
 
-
-
-// Permissions for the developer role - access to various AWS services
-
-resource "aws_iam_policy" "developer_role_access" {
-  name = "DeveloperAccess"
-  description = "Least privilege AWS access rights"
-  policy = "${data.aws_iam_policy_document.developer_role_access.json}"
+  assume_role_policy = "${data.aws_iam_policy_document.dev_trust_policy.json}"
 }
 
-data "aws_iam_policy_document" "developer_role_access" {
+data "aws_iam_policy_document" "dev_access" {
     statement {
     actions = [
       "s3:*",
+      "sqs:*",
+      "sns:*",
+      "lambda:*",
     ]
 
     resources = [
       "*",
     ]
+  }
+}
+
+resource "aws_iam_policy_attachment" "dev_attach" {
+  name       = "dev_attach"
+  groups     = ["${aws_iam_group.developers.name}"]
+  policy_arn = "${aws_iam_policy.developer_access.arn}"
+}
+
+// Developer role trust policy - which IAM groups / AWS accounts can assume the developer role
+data "aws_iam_policy_document" "dev_trust_policy" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    principals = [
+      {
+        type = "AWS"
+
+        identifiers = [
+          "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root",
+        ]
+      },
+    ]
+
+    condition = {
+      test     = "Bool"
+      variable = "aws:MultiFactorAuthPresent"
+      values   = ["true"]
+    }
   }
 }
