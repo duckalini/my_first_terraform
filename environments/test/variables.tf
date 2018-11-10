d81c426cdc69b90e04af2b5ce81c835df341fcea
Duck <duckalini@gmail.com>
2018-11-10 21:14:28 +1300

    Clarify bucket policy in 101 getting started module

--- environments/test/variables.tf
+++ environments/test/variables.tf
@@ -1,6 +1,3 @@
-# locals variables are really useful if you're going to be typing the same thing many times.
-# Find out more about them at https://www.terraform.io/docs/configuration/locals.html)
-
 locals {
   environment     = "test"
   project_name    = "my-first-terraform"
