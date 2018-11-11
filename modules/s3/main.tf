922b2b8bdf2ec09f25383081522ddaecbbc5c0d9
alix <alix.klingenberg@gmail.com>
2018-11-11 18:14:04 +1300

    S3 Bucket for user upload

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,23 +1,18 @@
-locals {
-  bucket_name = ["${var.environment}-upload-bucket"]
-}
-
 resource "aws_s3_bucket" "user_upload_bucket" {
-    bucket = ["${local.bucket_name}"]
+    bucket = "${var.environment}-upload-bucket"
     acl = "private"
 
-    policy = "${aws_iam_policy.bucket_policy}"
+    policy = "${aws_iam_policy.bucket_policy.arn}"
 }
 
 data "aws_iam_policy_document" "s3_bucket_policy" {
-
   statement {
+    effect = "Allow"
     actions = [
       "s3:*",
     ]
-
     resources = [
-      "arn:aws:s3:::${local.bucket_name}"
+      "arn:aws:s3:::${var.environment}-upload-bucket",
     ]
   }
 }
