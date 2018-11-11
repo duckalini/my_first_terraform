f5b08e382fc8942e91513e015cc3c505676ac1ac
alix <alix.klingenberg@gmail.com>
2018-11-11 19:32:45 +1300

    Add SNS resource for taking events from bucket

--- modules/sns/main.tf
+++ modules/sns/main.tf
@@ -1,11 +1,8 @@
 locals {
-  sns_topic_name = ["${var.environment}-upload-sns-topic"]
+  sns_topic_name = "${var.environment}-upload-sns-topic"
 }
 
 resource "aws_sns_topic" "upload-sns-topic" {
-  name = ["${local.sns_topic_name}"]
+  name = "${local.sns_topic_name}"
   //retry policy here might be nice
-}
-output "arn" {
-  value = "${aws_s3_bucket.cloudtrail_logs.id}"
 }
